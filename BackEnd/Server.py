2aa0c2d3f37fc83a0bb0ef231cf1781cec3e7177
Butterscotch <butterscotch@wireless-10-147-136-65.public.utexas.edu>
2018-10-20 17:21:09 -0500

    rev.ai works

--- BackEnd/Server.py
+++ BackEnd/Server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import requests
 import os 
+import json
 
 # defining constants
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -14,16 +15,58 @@ app = Flask(__name__)
 def lit():
     return "LITTTTT! This is bad ass"
 
-@app.route("/sample/")
-def samp1():
+@app.route("/sample/<int:rid>", methods=['GET'])
+def getJob(rid):
     # get file name
-    name = 'sample1.mp3'
+    name = 'sample' + str(rid) + '.mp3'
     filename = speechFolder + name
+    files = {}
     with open(filename, 'rb') as f:
         files = {'media': (name, f, 'audio/mp3')}
-        r = requests.post(api, headers=headers, files=files)
 
-    return str(r.content)
+        r = requests.post(api, headers=headers, files=files)
+        r = json.loads(r.content)
+
+        print(r)
+        if waitForTranscription(r['id']):
+            print("Waiting DONE!!!! " + str(rid))
+            return processTranscript(getTranscript(r['id']))
+
+        return "Waitin failed!"
+
+def processTranscript(transcript):
+    res = []
+    for t in transcript['monologues'][0]['elements']:
+        if t['type'] == 'text':
+            res.append(t['value'])
+    return ' '.join(res)
+
+
+def waitForTranscription(rid):
+    print(rid)
+    url = api + "/" + str(rid)
+    status = requests.get(url, headers = headers, params={'id': str(rid)})
+    status = json.loads(status.content)
+    print(status)
+    # if str(status.status_code) == '200':
+    while status['status'] == 'in_progress':
+        status = requests.get(url, headers = headers, params={'id': str(rid)})
+        status = json.loads(status.content)
+    return True
+    # return False
+
+
+def getTranscript(rid):
+    url = api + "/{_id}/transcript".format(_id=rid);
+    response = requests.get(url, headers = {'Accept': 'application/vnd.rev.transcript.v1.0+json', 'Authorization': headers['Authorization']}, params = {'id': str(rid)})
+    response = json.loads(response.content)
+    return response
+    
+
+
+def checkStatus(id):
+    r = requests.post(api + '/' + id, headers=headers)
+
 
 if __name__ == "__main__":
     app.run()
