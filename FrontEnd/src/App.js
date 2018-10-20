5d9973762aefb84294e8e2afecb496219fc02f9e
Chris Nunes <chrisnunes57@gmail.com>
2018-10-20 17:46:57 -0500

    Initial request + routing

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,79 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import LandingPage from "./LandingPage";
+import NotFound from "./NotFound";
+import Dashboard from "./Dashboard";
+
 
 class App extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            recorder: null,
-            recording: false
-        };
-        this.audioInput = React.createRef();
-        this.download = React.createRef();
-        this.button = React.createRef();
-    }
-
-    componentDidMount(){
-        this.download.current.download = 'audio.mp3';
-        // get audio stream from user's mic
-        navigator.mediaDevices.getUserMedia({
-            audio: true
-        }).then((stream) => {
-            this.setState({
-                recorder: new MediaRecorder(stream)
-            });
-
-            // listen to dataavailable, which gets triggered whenever we have
-            // an audio blob available
-            this.state.recorder.addEventListener('dataavailable', (e) => {this.onRecordingReady(e)});
-        });
-    };
-
-    startRecording(){
-        this.state.recorder.start();
-    }
-
-    stopRecording(){
-        this.state.recorder.stop();
-    }
-
-    // When we have an audio 'blob' that we can use, we create a URL so that it can be used.
-    onRecordingReady(e) {
-        let audio = this.audioInput.current;
-        // e.data contains a blob representing the recording
-        const blobURL = URL.createObjectURL(e.data);
-        audio.src = blobURL;
-        this.download.current.href = blobURL;
-        audio.play();
-    }
-
-    // This function toggles recording on/off, based on whether we're currently recording or not.
-    toggleRecordState(){
-        let button = this.button.current;
-        if(this.state.recording){
-            this.setState({recording: false});
-            button.className = "red";
-            this.stopRecording();
-        }else{
-            this.setState({recording: true});
-            button.className = "green";
-            this.startRecording();
-        }
-    }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-            <div className="navbar" />
-        </header>
-          <div className="content">
-              <button ref={this.button} className={this.state.recording ? "red" : "green"} onClick={() => {this.toggleRecordState()}}/>
-              <audio id="player" controls ref={this.audioInput}/>
-              <a href="#" ref={this.download}>Download</a>
-          </div>
-      </div>
+      <BrowserRouter>
+          <Switch>
+              <Route exact path="/" render={(props) => ( <LandingPage/> )}/>
+              <Route exact path="/dashboard" render={(props) => ( <Dashboard/> )}/>
+              <Route render={(props) => ( <NotFound/> )}/>
+          </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ FrontEnd/src/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class NotFound extends Component {
+
+  render() {
+    return (
+      <h1>
+          That's not a real link, dumbass
+      </h1>
+    );
+  }
+}
+
+export default NotFound;
