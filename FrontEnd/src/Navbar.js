bae3e6d2a41d3540a5b85f284c4e823a25e84a09
Chris Nunes <chrisnunes57@gmail.com>
2018-10-20 18:19:25 -0500

    Navbar

---
+++ FrontEnd/src/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Link} from "react-router-dom";
+
+
+
+class Navbar extends Component {
+
+    render() {
+        return (
+            <header className="App-header">
+                <div className="navbar">
+                    <div className="nav-left">
+                        <h1 style={{cursor: "pointer"}} onClick={() => {window.location="/"}}>HackTX 2018</h1>
+                    </div>
+                     <div className="nav-right">
+                        <h1>Other stuff here?</h1>
+                     </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Navbar;
