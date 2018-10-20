cb5d198e40aa2cb895de0d4dd73a6793d29e495d
lbn99 <lyndonbn@gmail.com>
2018-10-20 16:36:11 -0500

    working process of audio file

--- rev_ai_test/rev_s2t.py
+++ rev_ai_test/rev_s2t.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 url = "https://api.rev.ai/revspeech/v1beta/jobs"
 data = {"media_url": "https://support.rev.com/hc/en-us/article_attachments/200043975/FTC_Sample_1_-_Single.mp3",
@@ -13,18 +14,24 @@ print("Got ID response from rev.ai")
 text = json.loads(res.text)
 req_id = text["id"]
 
-# print(text)
-# print(req_id)
+param = {"id": str(req_id)}
+print("Waiting to get the transcript...")
+start = time.time()
+
+# check the status until the transcript is ready
+stat_url = "https://api.rev.ai/revspeech/v1beta/jobs/" + str(req_id)
+stat_headers = {"Authorization": "Bearer 01x_We_kYHsVnoJnVWfrJIfrzPsLSvmTDmOJcg3_XsLTsX9OV_GglZA6RMz4ohUl2BkKMjDrZjb_HgszMnzKY0S3RjHjU"}
+res = requests.get(stat_url, headers=stat_headers, params=param)
+stat = json.loads(res.text)["status"]
+while stat == "in_progress":
+    res = requests.get(stat_url, headers=stat_headers, params=param)
+    stat = json.loads(res.text)["status"]
+end = time.time()
+
 
 get_url = "https://api.rev.ai/revspeech/v1beta/jobs/" + str(req_id) + "/transcript"
 get_headers = {"Authorization": "Bearer 01x_We_kYHsVnoJnVWfrJIfrzPsLSvmTDmOJcg3_XsLTsX9OV_GglZA6RMz4ohUl2BkKMjDrZjb_HgszMnzKY0S3RjHjU",
                 "Accept": "text/plain"}
-alt = {"Authorization": "Bearer 01x_We_kYHsVnoJnVWfrJIfrzPsLSvmTDmOJcg3_XsLTsX9OV_GglZA6RMz4ohUl2BkKMjDrZjb_HgszMnzKY0S3RjHjU",
-        "Accept": "application/vnd.rev.transcript.v1.0+json"} 
-param = {"id": str(req_id)}
-
-print("Waiting to get the transcript...")
-# while res != "":
 res = requests.get(get_url, headers=get_headers, params=param)
-print("Transcript received!")
-print(str(res) + " " + res.text)
+print("Transcript received! Elapsed time: " + str(end - start))
+print(res.text)
