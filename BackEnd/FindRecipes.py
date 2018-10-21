ebfaca35462aa08e17441167032ae2de835cb008
Chris Nunes <chrisnunes57@gmail.com>
2018-10-21 11:55:46 -0500

    fixed

--- BackEnd/FindRecipes.py
+++ BackEnd/FindRecipes.py
@@ -5,10 +5,10 @@ import requests
 def findPrice(product):
     product_str = '%20'.join(product.split(' '))
     response = requests.get('https://www.walmart.com/search/?query=' + product_str)
-    
+
     soup = BeautifulSoup(response.content, 'html.parser')
     grps = soup.find_all('span', {'class': 'price-group'})
-    
+
     groups_str = ''
     for g in grps:
         groups_str += str(g)
@@ -17,7 +17,7 @@ def findPrice(product):
     rm = ['0.00', '$1.00', '$1.0', '$0.00', '$1.1']
     for v in rm:
         m = list(filter(lambda a: a != v, m))
-    
+
     res = []
     for v in m:
             v = v[1:]
@@ -26,10 +26,8 @@ def findPrice(product):
             except:
                 continue
     res.sort()
-    res = res[int(len(res)*.13): int(len(res)*.4)]
+    res = res[int(len(res)*.03): int(len(res)*.24)]
 
     if res:
         return sum(res)/len(res)
     return 0
-
-
